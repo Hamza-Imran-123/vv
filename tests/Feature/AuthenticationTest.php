c980cf43c6fe4f0710d41feb6102f3ed86e184c5
Ubuntu <ubuntu@ip-172-31-21-102.ec2.internal>
2025-01-04 13:56:20 +0000

    Update GitHub Actions to use Pest for testing

--- tests/Feature/AuthenticationTest.php
+++ tests/Feature/AuthenticationTest.php
@@ -1,13 +1,25 @@
-<?php
+use App\Models\User;
 
-test('Test login screen can be rendered')->get('/login')->assertStatus(200);
+test('Test login screen can be rendered', function () {
+    $this->get('/login')->assertStatus(200);
+});
 
 test('Test users can authenticate using the login screen', function () {
-    $response = $this->post('/login', [
+    // Ensure the user exists in the database
+    $user = User::factory()->create([
         'email' => 'user@gmail.com',
-        'password' => 'password',
+        'password' => bcrypt('password'), // Ensure the password is hashed
     ]);
 
-    $this->assertAuthenticated();
+    // Attempt to log in
+    $response = $this->post('/login', [
+        'email' => 'user@gmail.com',
+        'password' => 'password', // The plain text password
+    ]);
+
+    // Assert that the user is authenticated
+    $this->assertAuthenticatedAs($user);
+    // Assert that the user is redirected to the movies page
     $response->assertRedirect('/movies');
 });
+
